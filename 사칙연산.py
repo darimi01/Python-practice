d2ca003db842355bcd0fdce3d8c8379e7b561601
darimi01 <133104027+darimi01@users.noreply.github.com>
2023-11-23 15:40:34 +0900

    Add files via upload

---
+++ 사칙연산.py
@@ -0,0 +1,19 @@
+#사칙연산
+
+#두 정수 A와B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+a,b = map(int,input().split())
+print('a+b = ', a+b)
+
+#두 정수 A와B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+a,b =map(int,input().split())
+print(a-b)
+
+#두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+a,b =map(int,input().split())
+print(a*b)
+
+#두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+a,b =map(int,input().split())
+print(a/b)
+
+#/는 몫 %는 나머지
